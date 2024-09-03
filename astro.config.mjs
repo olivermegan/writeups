c8464d1d7e9532f473cb964967df1205faaf45d7
Oliver Megan <oliver.megan123@gmail.com>
2024-09-03 13:33:57 +0100

    mdx integration support added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 //import sitemap from "@astrojs/sitemap";
 import tailwind from '@astrojs/tailwind';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://olivermegan.github.io',
     base: '/writeups',
-    integrations: [tailwind()]
+    integrations: [tailwind(), mdx()]
 });
