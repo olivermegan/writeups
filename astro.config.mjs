8c7662354a0d6ab6b7b08d12ccaea26823f074e8
Oliver Megan <oliver.megan123@gmail.com>
2024-09-03 13:51:25 +0100

    test machine.md added and astro.config modified for mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-//import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 //import sitemap from "@astrojs/sitemap";
 import tailwind from '@astrojs/tailwind';
 
