9ba1aaf0d560ec31cb813bcf2e989116ea715a20
Oliver Megan <oliver.megan123@gmail.com>
2024-09-17 14:29:58 +0100

    base url restored

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://olivermegan.github.io/writeups',
-    base: '/home',
+    base: '/writeups',
     integrations: [tailwind(), mdx()]
 });
